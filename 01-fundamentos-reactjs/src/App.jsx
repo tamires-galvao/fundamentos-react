cb6f6b0abd159d1f0bce907c720d37ccb4ff4aad
Tamires Moreno <tamiresmorenogalvao@gmail.com>
2023-10-09 21:14:28 -0300

    Iniciando css Sidebar

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,19 +1,27 @@
 import { Header } from './components/Header'
 import {Post} from './Post'
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
       <Header/>
-      <Post 
-        author = 'Tamires'
-        content = 'kLorem ipsum dolor sit amet consectetur adipisicing elit. Vero aliquid quisquam adipisci ipsam dolores, nisi nostrum, hic et quasi, molestiae reiciendis. Molestiae minima nobis tempore qui praesentium magni aliquam facere!'
-      />
-      <Post 
-        author = 'Kamila'
-        content = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero aliquid quisquam adipisci ipsam dolores, nisi nostrum, hic et quasi, molestiae reiciendis. Molestiae minima nobis tempore qui praesentium magni aliquam facere!'
-      />
+     <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        <Post
+          author="Tamires Moreno"
+          content= "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        />
+
+        <Post
+          author="Kamila Bordin"
+          content= "Um novo Post Legal"
+        />
+      </main>
+     </div>
     </div>
   )
 }
