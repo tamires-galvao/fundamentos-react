1e108301aa201ed6500ee5178ce079f83caa2bdf
Tamires Moreno <tamiresmorenogalvao@gmail.com>
2023-12-28 18:32:27 -0300

    Propriedades do Post

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -43,7 +43,14 @@ export function App() {
       <Sidebar />
       <main>
         {posts.map(post => {
-          return <Post/>
+          return (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
         })}
       </main>
      </div>
--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,35 +1,39 @@
-import { Avatar } from './Avatar'
-import { Comment } from './Comment'
-import styles from './Post.module.css'
-export function Post(props) {
-  console.log(props)
+/* eslint-disable react/prop-types */
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+import styles from './Post.module.css';
+export function Post({author, publishedAt, content}) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
+    locale: ptBR,
+  })
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
   return(
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src='https://github.com/tamires-galvao.png'/>
+          <Avatar src={author.avatarUrl}/>
           <div className={styles.authorInfo}>
-            <strong>Tamires Moreno</strong>
-            <span>Full Stack Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
-        <time title='01 de Novembro ás 20:33' dateTime='2023-11-01 20:32:00'>Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
       <div className={styles.content}>
-          <p>
-            Fala galeraa 👋
-          </p>   
-          <p>
-            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return,  evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-          </p>
-          <p>
-            <a href=''>jane.design/doctorcare</a>
-          </p>
-          <p>
-            <a href=''>#novoprojeto</a>{' '}
-            <a href=''>#nlw</a>{' '}
-            <a href=''>#rocketseat</a>{' '}
-            </p>
+         {content.map(line => {
+           if(line.type === 'paragraph'){
+            return <p key={line.index}>{line.content}</p>;
+           } else if (line.type === 'link'){
+            return <p key={line.index}><a>{line.content}</a></p>;
+           }
+         })}
       </div>
 
       <form className={styles.commentForm}>
