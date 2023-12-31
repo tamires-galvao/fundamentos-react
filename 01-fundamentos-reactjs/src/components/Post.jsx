d51a09de00626b5cedfb32130d272524f58f8b39
Tamires Moreno <tamiresmorenogalvao@gmail.com>
2023-12-31 12:25:06 -0300

    Ajustando keys

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -48,9 +48,9 @@ export function Post({author, publishedAt, content}) {
       <div className={styles.content}>
          {content.map(line => {
            if(line.type === 'paragraph'){
-            return <p key={line.index}>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
            } else if (line.type === 'link'){
-            return <p key={line.index}><a>{line.content}</a></p>;
+            return <p key={line.content}><a>{line.content}</a></p>;
            }
          })}
       </div>
@@ -69,7 +69,7 @@ export function Post({author, publishedAt, content}) {
       </form>
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment.id} content={comment}/>
+          return <Comment key={comment} content={comment}/>
         })}
       </div>
     </article>
